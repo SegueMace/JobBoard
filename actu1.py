6d2d90b03ff0cfe893a6e7e212f5c86b9c56e15f
Segue Mace <seguemace@gmail.com>
2022-05-08 02:24:04 +0100

    all

---
+++ actu1.py
@@ -0,0 +1,21 @@
+from requests_html import HTMLSession
+
+session = HTMLSession()
+
+url = 'https://news.google.com/topstories?hl=fr&gl=FR&ceid=FR%3Afr'
+
+
+r = session.get(url)
+
+r.html.render(sleep=1, scrolldown=5)
+
+articles = r.html.find('article')
+
+print(articles)
+
+for item in articles:
+    newsitem = item.find('h3', first=True)
+    title = newsitem.text
+    link = newsitem.absolute_links
+    print(title, link)
+
